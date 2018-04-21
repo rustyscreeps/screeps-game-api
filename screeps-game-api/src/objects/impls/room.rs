871ec60f1e42ebb23467aecb26ea5d2b794f1d9c
David Ross <daboross@daboross.net>
2018-04-20 23:37:32 -0700

    Implement a prototype look_for_at_area.

--- screeps-game-api/src/objects/impls/room.rs
+++ screeps-game-api/src/objects/impls/room.rs
@@ -80,6 +80,47 @@ impl Room {
         ))
     }
 
+    /// Looks for a given thing over a given area of bounds.
+    ///
+    /// To keep with `Range` convention, the start is inclusive, and the end
+    /// is _exclusive_.
+    ///
+    /// Note: to ease the implementation and efficiency of the rust interface, this is limited to
+    /// returning an array of values without their positions. If position data is needed, all room
+    /// objects *should* contain positions alongside them. (for terrain data, I would recommend
+    /// using a different method?)
+    ///
+    /// If you really do need more information here, I would recommend making a PR to add it!
+    ///
+    /// # Panics
+    ///
+    /// Panics if start>end for either range, or if end>50 for either range.
+    ///
+    /// # Examples
+    ///
+    /// ```ignore
+    /// use screeps::constants::look;
+    /// room.look_for_at_area(look::ENERGY, 20..26, 20..26)
+    /// ```
+    pub fn look_for_at_area<T>(&self, ty: T, horiz: Range<u8>, vert: Range<u8>) -> Vec<T::Item>
+    where
+        T: LookConstant,
+    {
+        assert!(horiz.start <= horiz.end);
+        assert!(vert.start <= vert.end);
+        assert!(horiz.end <= 50);
+        assert!(vert.end <= 50);
+
+        js_unwrap_array!(@{self.as_ref()}.lookForAtArea(
+            __look_num_to_str(@{ty.look_code() as i32}),
+            @{vert.start},
+            @{horiz.start},
+            @{vert.end},
+            @{horiz.end},
+            true
+        ).map((obj) => obj[__look_num_to_str(@{ty.look_code() as i32})]))
+    }
+
     pub fn name_local(&self) -> LocalRoomName {
         js_unwrap!(@{self.as_ref()}.name)
     }
