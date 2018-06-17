45f324f0f779713a89d764c1c42215ac6339c052
David Ross <daboross@daboross.net>
2018-06-16 23:25:42 -0700

    Update pre-set suffix.
    Fixes https://github.com/daboross/screeps-in-rust-via-wasm/issues/13.

--- cargo-screeps/src/build.rs
+++ cargo-screeps/src/build.rs
@@ -168,7 +168,7 @@ if( typeof Rust === "undefined" ) {
 
         return __initialize( mod, false );
     } else {
-        return fetch( "XXX.wasm" )
+        return fetch( "XXX.wasm", {credentials: "same-origin"} )
             .then( response => response.arrayBuffer() )
             .then( bytes => WebAssembly.compile( bytes ) )
             .then( mod => __initialize( mod, true ) );
