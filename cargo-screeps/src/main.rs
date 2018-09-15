6bdec102ac24c44537dc9719653ef22a21766493
David Ross <daboross@daboross.net>
2018-09-15 00:25:53 -0700

    Improve error reporting.
    This adds showing error causes, which is helpful, and removes the empty
    backtrace line so it isn't shown unless backtraces are actually enabled
    with RUST_BACKTRACE=1.

--- cargo-screeps/src/main.rs
+++ cargo-screeps/src/main.rs
@@ -26,8 +26,15 @@ mod upload;
 
 fn main() {
     if let Err(e) = run::run() {
-        eprintln!("{}", e.backtrace());
         eprintln!("error: {}", e);
+        for cause in e.iter_causes() {
+            eprintln!("  ⬑ {}", cause);
+        }
+        let backtrace = format!("{}", e.backtrace());
+        // don't print an empty backspace line if it's not enabled.
+        if backtrace.trim() != "" {
+            eprintln!("{}", backtrace);
+        }
         std::process::exit(1);
     }
 }
