2d0735dac1cdc28ce4405234899022248ad322a9
Adam Salvail <ausprit@gmail.com>
2018-09-16 01:31:44 -0400

    Add structure_nuker mod + implement methods

---
+++ screeps-game-api/src/objects/impls/structure_nuker.rs
@@ -0,0 +1,16 @@
+use {
+    constants::ReturnCode,
+    {RoomPosition, StructureNuker},
+};
+
+simple_accessors! {
+    StructureNuker;
+    (ghodium -> ghodium -> u32),
+    (ghodium_capacity -> ghodiumCapacity -> u32),
+}
+
+impl StructureNuker {
+    pub fn launch_nuke(&self, pos: &RoomPosition) -> ReturnCode {
+        js_unwrap!{@{self.as_ref()}.launchNuke(@{pos.as_ref()})}
+    }
+}
