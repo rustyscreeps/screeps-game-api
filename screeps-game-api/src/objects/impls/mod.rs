0dde821ea31c746ab1602d49d58ae11c4f5e4207
Adam Salvail <ausprit@gmail.com>
2018-09-16 03:06:27 -0400

    Import all changes into impls module

--- screeps-game-api/src/objects/impls/mod.rs
+++ screeps-game-api/src/objects/impls/mod.rs
@@ -1,15 +1,27 @@
-#[macro_use]
-mod utils;
 mod construction_site;
 mod container;
 mod creep;
+mod flag;
+mod mineral;
+mod nuke;
 mod resource;
-mod room;
 mod room_position;
+mod room;
 mod source;
 mod structure_controller;
+mod structure_lab;
+mod structure_link;
+mod structure_nuker;
+mod structure_observer;
+mod structure_portal;
+mod structure_power_spawn;
+mod structure_rampart;
 mod structure_spawn;
+mod structure_terminal;
+mod structure_tower;
 mod tombstone;
+#[macro_use]
+mod utils;
 
 pub use self::structure_controller::{Reservation, Sign};
 pub use self::structure_spawn::SpawnOptions;
