5f092234c3adcc5f5c2dada4a5de5300491be0e1
Adam Salvail <ausprit@gmail.com>
2018-10-05 03:22:27 -0400

    Implements passing vector to get_raw_buffer

--- screeps-game-api/src/objects/impls/room_terrain.rs
+++ screeps-game-api/src/objects/impls/room_terrain.rs
@@ -1,15 +1,37 @@
-use {constants::Terrain, objects::RoomTerrain};
+use stdweb::UnsafeTypedArray;
+
+use {constants::{ReturnCode, Terrain}, objects::RoomTerrain, traits::TryInto};
 
 impl RoomTerrain {
     pub fn constructor(room_name: &str) -> Self {
-        js_unwrap!{new Room.Terrain(@{room_name})}
+        js_unwrap!(new Room.Terrain(@{room_name}))
     }
 
     pub fn get(&self, x: u32, y: u32) -> Terrain {
-        js_unwrap!{@{self.as_ref()}.get(@{x}, @{y})}
+        js_unwrap!(@{self.as_ref()}.get(@{x}, @{y}))
     }
 
     pub fn get_raw_buffer(&self) -> Vec<u8> {
-        js_unwrap!(@{self.as_ref()}.getRawBuffer())
+        let mut buffer: Vec<u8> = vec![0; 2500];
+        self.get_raw_buffer_array_ref(&mut buffer).expect("Panic in get_raw_buffer.");
+        buffer
     }
+
+    pub fn get_raw_buffer_array_ref<'a>(&self, buffer: &'a mut Vec<u8>) -> Result<&'a mut Vec<u8>, ReturnCode> {
+        let is_success: bool;
+        {
+            let arr: UnsafeTypedArray<u8> = unsafe{ UnsafeTypedArray::new(buffer.as_mut_slice()) };
+
+            is_success = js!{
+                var bytes = @{arr};
+                return @{self.as_ref()}.getRawBuffer(bytes) != -10;
+            }.try_into().unwrap();
+        }
+        if is_success {
+            Ok(buffer)
+        } else {
+            Err(ReturnCode::InvalidArgs)
+        }
+    }
+
 }
