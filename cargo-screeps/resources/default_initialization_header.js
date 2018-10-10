61ac83e2b7ca4a9b86507c74b3ef729bd0e50985
David Ross <daboross@daboross.net>
2018-10-10 15:03:57 -0700

    Allow main loop to run in the same tick WASM loads

--- cargo-screeps/resources/default_initialization_header.js
+++ cargo-screeps/resources/default_initialization_header.js
@@ -9,6 +9,8 @@ function wasm_initialize() {
     let stdweb_vars = wasm_create_stdweb_vars();
     let wasm_instance = new WebAssembly.Instance(wasm_module, stdweb_vars.imports);
     stdweb_vars.initialize(wasm_instance);
+    // assume the WASM main overrides this
+    module.exports.loop();
 }
 
 module.exports.loop = wasm_initialize;
