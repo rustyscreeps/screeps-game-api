7e8a02de9a1521464cd56e83c5fabffbfde1def9
David Ross <daboross@daboross.net>
2018-11-04 02:52:21 -0800

    Format flag::set_color and include semicolons.
    Honestly this doesn't matter at all but I think it might look a bit
    better? At least this matches the rustfmt style of having parameters be
    a block rather than aligned with eachother - rustfmt won't touch macro
    internals so this needs to be done manually.

--- screeps-game-api/src/objects/impls/flag.rs
+++ screeps-game-api/src/objects/impls/flag.rs
@@ -17,10 +17,12 @@ impl Flag {
 
     pub fn set_color(&self, color: Color, secondary_color: Option<Color>) {
         match secondary_color {
-            None => js! {@{self.as_ref()}.setColor(@{u32::from(color)})},
+            None => js! {@{self.as_ref()}.setColor(@{u32::from(color)});},
             Some(sec_color) => js! {
-                @{self.as_ref()}.setColor(@{u32::from(color)},
-                                          @{u32::from(sec_color)})
+                @{self.as_ref()}.setColor(
+                    @{u32::from(color)},
+                    @{u32::from(sec_color)},
+                );
             },
         };
     }
