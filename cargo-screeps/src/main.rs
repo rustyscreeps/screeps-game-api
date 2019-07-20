fffe4efd5e9503cd6a11b9baaafce31320f87d51
David Ross <daboross@daboross.net>
2019-07-20 14:45:33 -0700

    Remove easy-to-remove extern crate statements.

--- cargo-screeps/src/main.rs
+++ cargo-screeps/src/main.rs
@@ -1,21 +1,15 @@
-extern crate base64;
 #[macro_use]
 extern crate clap;
 #[macro_use]
 extern crate failure;
-extern crate fern;
+
 #[macro_use]
 extern crate log;
-extern crate pathdiff;
-extern crate regex;
-extern crate reqwest;
-extern crate serde;
+
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
-extern crate serde_ignored;
-extern crate toml;
 
 mod build;
 mod config;
