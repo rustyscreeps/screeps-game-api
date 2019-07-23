31bec765850e86a8144751c4c8b1a867ca0feec8
David Ross <daboross@daboross.net>
2019-07-23 13:43:06 -0700

    Improve macro doc comments

--- src/macros.rs
+++ src/macros.rs
@@ -342,9 +342,9 @@ macro_rules! creep_simple_concrete_action {
     )
 }
 
-/// Declares a unit structure with a doc attribute which is computed by a macro
-/// expression. This allows documentation to be dynamically generated, for
-/// instance. Necessary to work around https://github.com/rust-lang/rust/issues/52607.
+/// Declares an item with a doc attribute computed by some macro expression.
+/// This allows documentation to be dynamically generated based on input.
+/// Necessary to work around https://github.com/rust-lang/rust/issues/52607.
 macro_rules! calculated_doc {
     (
         $(
