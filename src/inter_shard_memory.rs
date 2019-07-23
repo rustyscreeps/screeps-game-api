ea6c4f35bd26550e2d6f36ff1bbd8b7f99a6712e
David Ross <daboross@daboross.net>
2019-07-22 19:20:27 -0700

    Improve inter_shard_memory documentation.

--- src/inter_shard_memory.rs
+++ src/inter_shard_memory.rs
@@ -1,18 +1,20 @@
-//! `inter_shard_memory` provides an interface for communciating between shards.
+//! An interface for communicating between shards.
 //!
-//! Quoted from https://docs.screeps.com/api/#InterShardMemory:
+//! Quoting from [`InterShardMemory`] documentation::
 //!
-//! InterShardMemory object provides an interface for communicating between
-//! shards. Your script is executed separatedly on each shard, and their Memory
-//! objects are isolated from each other. In order to pass messages and data
-//! between shards, you need to use InterShardMemory instead.
+//! > InterShardMemory object provides an interface for communicating between
+//! > shards. Your script is executed separatedly on each shard, and their Memory
+//! > objects are isolated from each other. In order to pass messages and data
+//! > between shards, you need to use InterShardMemory instead.
+//! >
+//! > Every shard can have its own data string that can be accessed by all other
+//! > shards. A shard can write only to its own data, other shards' data is
+//! > read-only.
+//! >
+//! > This data has nothing to do with Memory contents, it's a separate data
+//! > container.
 //!
-//! Every shard can have its own data string that can be accessed by all other
-//! shards. A shard can write only to its own data, other shards' data is
-//! read-only.
-//!
-//! This data has nothing to do with Memory contents, it's a separate data
-//! container.
+//! [`InterShardMemory`]: https://docs.screeps.com/api/#InterShardMemory
 use crate::macros::*;
 
 /// Returns the string contents of the current shard's data.
