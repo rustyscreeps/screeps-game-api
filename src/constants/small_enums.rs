35529f0da7ac7d72b23f93dcf94df793817a401d
David Ross <daboross@daboross.net>
2019-08-21 23:46:35 -0700

    Split constants.rs into multiple files
    This is hopefully nice for maintenance, and contains no functional
    changes other than `Look` and `LookConstant` being accessible from the
    `look` module (previously, they were outside the module - now they're
    inside and also re-exported outside)

---
+++ src/constants/small_enums.rs
@@ -0,0 +1,344 @@
+//! Various constants translated as small enums.
+use std::{borrow::Cow, fmt, str::FromStr};
+
+use enum_iterator::IntoEnumIterator;
+use num_derive::FromPrimitive;
+use parse_display::FromStr;
+use serde::{
+    de::{Deserializer, Error as _, Unexpected},
+    Deserialize, Serialize,
+};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+use super::{
+    find,
+    numbers::{TERRAIN_MASK_SWAMP, TERRAIN_MASK_WALL},
+};
+use crate::macros::*;
+
+#[derive(
+    Debug, PartialEq, Eq, Clone, Copy, FromPrimitive, Hash, Deserialize_repr, Serialize_repr,
+)]
+#[repr(i16)]
+pub enum ReturnCode {
+    Ok = 0,
+    NotOwner = -1,
+    NoPath = -2,
+    NameExists = -3,
+    Busy = -4,
+    NotFound = -5,
+    NotEnough = -6,
+    InvalidTarget = -7,
+    Full = -8,
+    NotInRange = -9,
+    InvalidArgs = -10,
+    Tired = -11,
+    NoBodypart = -12,
+    RclNotEnough = -14,
+    GclNotEnough = -15,
+}
+
+impl ReturnCode {
+    /// Turns this return code into a result.
+    ///
+    /// `ReturnCode::Ok` is turned into `Result::Ok`, all other codes are turned
+    /// into `Result::Err(code)`
+    #[inline]
+    pub fn as_result(self) -> Result<(), Self> {
+        match self {
+            ReturnCode::Ok => Ok(()),
+            other => Err(other),
+        }
+    }
+}
+
+js_deserializable!(ReturnCode);
+
+#[derive(
+    Debug, PartialEq, Eq, Clone, Copy, Hash, FromPrimitive, Serialize_repr, Deserialize_repr,
+)]
+#[repr(u8)]
+pub enum Direction {
+    Top = 1,
+    TopRight = 2,
+    Right = 3,
+    BottomRight = 4,
+    Bottom = 5,
+    BottomLeft = 6,
+    Left = 7,
+    TopLeft = 8,
+}
+
+js_deserializable!(Direction);
+
+impl ::std::ops::Neg for Direction {
+    type Output = Direction;
+
+    /// Negates this direction. Top goes to Bottom, TopRight goes to BottomLeft,
+    /// etc.
+    ///
+    /// Example usage:
+    ///
+    /// ```
+    /// use screeps::Direction::*;
+    ///
+    /// assert_eq!(-Top, Bottom);
+    /// assert_eq!(-BottomRight, TopLeft);
+    /// assert_eq!(-Left, Right);
+    /// ```
+    #[inline]
+    fn neg(self) -> Direction {
+        use crate::Direction::*;
+
+        match self {
+            Top => Bottom,
+            TopRight => BottomLeft,
+            Right => Left,
+            BottomRight => TopLeft,
+            Bottom => Top,
+            BottomLeft => TopRight,
+            Left => Right,
+            TopLeft => BottomRight,
+        }
+    }
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let ch = match *self {
+            Direction::Top => "↑",
+            Direction::TopRight => "↗",
+            Direction::Right => "→",
+            Direction::BottomRight => "↘",
+            Direction::Bottom => "↓",
+            Direction::BottomLeft => "↙",
+            Direction::Left => "←",
+            Direction::TopLeft => "↖",
+        };
+        f.write_str(ch)
+    }
+}
+
+/// Type used for when the game returns a direction to an exit.
+///
+/// Restricted more than `Direction` in that it can't be diagonal. Used as the
+/// result of [`Room::find_exit_to`].
+///
+/// Can be converted to both [`find::Exit`] for immediate use of [`Room::find`]
+/// and [`Direction`].
+///
+/// [`Room::find`]: crate::objects::Room::find
+/// [`Room::find_exit_to`]: crate::objects::Room::find_exit_to
+#[derive(
+    Copy, Clone, Debug, FromPrimitive, Deserialize_repr, Serialize_repr, PartialEq, Eq, Hash,
+)]
+#[repr(u8)]
+pub enum ExitDirection {
+    Top = Direction::Top as u8,
+    Right = Direction::Right as u8,
+    Bottom = Direction::Bottom as u8,
+    Left = Direction::Left as u8,
+}
+
+impl From<ExitDirection> for find::Exit {
+    #[inline]
+    fn from(dir: ExitDirection) -> Self {
+        match dir {
+            ExitDirection::Top => find::Exit::Top,
+            ExitDirection::Right => find::Exit::Right,
+            ExitDirection::Bottom => find::Exit::Bottom,
+            ExitDirection::Left => find::Exit::Left,
+        }
+    }
+}
+
+impl From<ExitDirection> for Direction {
+    #[inline]
+    fn from(dir: ExitDirection) -> Self {
+        match dir {
+            ExitDirection::Top => Direction::Top,
+            ExitDirection::Right => Direction::Right,
+            ExitDirection::Bottom => Direction::Bottom,
+            ExitDirection::Left => Direction::Left,
+        }
+    }
+}
+
+#[derive(
+    Debug, PartialEq, Eq, Clone, Copy, FromPrimitive, Hash, Deserialize_repr, Serialize_repr,
+)]
+#[repr(u8)]
+pub enum Color {
+    Red = 1,
+    Purple = 2,
+    Blue = 3,
+    Cyan = 4,
+    Green = 5,
+    Yellow = 6,
+    Orange = 7,
+    Brown = 8,
+    Grey = 9,
+    White = 10,
+}
+
+js_deserializable!(Color);
+
+/// Terrain constant.
+///
+/// This constant is in a unique position of being represented both by strings
+/// and by integers in various parts of the API.
+///
+/// *Note:* This constant's `TryFrom<Value>` and `Deserialize` implementations
+/// _only work with the integer constants_. If you're ever consuming strings
+/// such as `"plain"`, `"swamp"`, `"wall"`, you can use the
+/// `__terrain_str_to_num` JavaScript function, [`FromStr`][std::str::FromStr]
+/// or [`Look::deserialize_from_str`].
+///
+/// See the [module-level documentation][crate::constants] for more details.
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash,
+    Deserialize_repr,
+    Serialize_repr,
+    FromPrimitive,
+    FromStr,
+)]
+#[repr(u8)]
+#[display(style = "snake_case")]
+pub enum Terrain {
+    Plain = 0,
+    Wall = TERRAIN_MASK_WALL,
+    Swamp = TERRAIN_MASK_SWAMP,
+}
+
+impl Terrain {
+    /// Helper function for deserializing from a string rather than from an
+    /// integer.
+    pub fn deserialize_from_str<'de, D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let s: Cow<'de, str> = Cow::deserialize(d)?;
+        Self::from_str(&s).map_err(|_| {
+            D::Error::invalid_value(Unexpected::Str(&s), &r#""plain", "wall" or "swamp""#)
+        })
+    }
+}
+
+js_deserializable!(Terrain);
+
+/// Creep part types.
+///
+/// *Note:* This constant's `TryFrom<Value>`, `Serialize` and `Deserialize`
+/// implementations only operate on made-up integer constants. If you're ever
+/// using these impls manually, use the `__part_num_to_str` and
+/// `__part_str_to_num` JavaScript functions, [`FromStr`][std::str::FromStr] or
+/// [`Part::deserialize_from_str`].
+///
+/// See the [module-level documentation][crate::constants] for more details.
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Serialize_repr, Deserialize_repr, FromStr)]
+#[repr(u8)]
+#[display(style = "snake_case")]
+pub enum Part {
+    Move = 0,
+    Work = 1,
+    Carry = 2,
+    Attack = 3,
+    RangedAttack = 4,
+    Tough = 5,
+    Heal = 6,
+    Claim = 7,
+}
+
+impl Part {
+    /// Translates the `BODYPART_COST` constant.
+    #[inline]
+    pub fn cost(self) -> u32 {
+        match self {
+            Part::Move => 50,
+            Part::Work => 100,
+            Part::Carry => 50,
+            Part::Attack => 80,
+            Part::RangedAttack => 150,
+            Part::Tough => 10,
+            Part::Heal => 250,
+            Part::Claim => 600,
+        }
+    }
+
+    /// Helper function for deserializing from a string rather than a fake
+    /// integer value.
+    pub fn deserialize_from_str<'de, D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let s: Cow<'de, str> = Cow::deserialize(d)?;
+        Self::from_str(&s).map_err(|_| {
+            D::Error::invalid_value(
+                Unexpected::Str(&s),
+                &"a known constant string in BODYPARTS_ALL",
+            )
+        })
+    }
+}
+
+js_deserializable!(Part);
+
+/// Translates the `DENSITY_*` constants.
+#[derive(
+    Debug,
+    PartialEq,
+    Eq,
+    Clone,
+    Copy,
+    FromPrimitive,
+    Hash,
+    Serialize_repr,
+    Deserialize_repr,
+    IntoEnumIterator,
+)]
+#[repr(u8)]
+pub enum Density {
+    Low = 1,
+    Moderate = 2,
+    High = 3,
+    Ultra = 4,
+}
+
+js_deserializable!(Density);
+
+impl Density {
+    /// Translates the `MINERAL_DENSITY` constant.
+    #[inline]
+    pub fn amount(self) -> u32 {
+        match self {
+            Density::Low => 15_000,
+            Density::Moderate => 35_000,
+            Density::High => 70_000,
+            Density::Ultra => 100_000,
+        }
+    }
+
+    /// Translates the `MINERAL_DENSITY_PROBABILITY` constant.
+    ///
+    /// These are values intended for subsequent percentage checks
+    /// in the order `Low` -> `Medium` -> `High` -> `Ultra`. Use the
+    /// [`Density::iter_values`] iterator to iterate in this order.
+    #[inline]
+    pub fn probability(self) -> f32 {
+        match self {
+            Density::Low => 0.1,
+            Density::Moderate => 0.5,
+            Density::High => 0.9,
+            Density::Ultra => 1.0,
+        }
+    }
+
+    pub fn iter_values() -> impl Iterator<Item = Density> {
+        <Density as enum_iterator::IntoEnumIterator>::into_enum_iter()
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum PowerClass {
+    Operator,
+}
---
+++ src/constants/types.rs
@@ -0,0 +1,415 @@
+//! `*Type` constants.
+use std::{borrow::Cow, str::FromStr};
+
+use num_derive::FromPrimitive;
+use parse_display::FromStr;
+use serde::{
+    de::{Deserializer, Error as _, Unexpected},
+    Deserialize,
+};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+use crate::macros::*;
+
+/// Translates `STRUCTURE_*` constants.
+///
+/// *Note:* This constant's `TryFrom<Value>`, `Serialize` and `Deserialize`
+/// implementations only operate on made-up integer constants. If you're ever
+/// using these impls manually, use the `__structure_type_num_to_str` and
+/// `__structure_type_str_to_num` JavaScript functions,
+/// [`FromStr`][std::str::FromStr] or [`StructureType::deserialize_from_str`].
+///
+/// See the [module-level documentation][crate::constants] for more details.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize_repr, Deserialize_repr, FromStr)]
+#[repr(u8)]
+#[display(style = "camelCase")]
+pub enum StructureType {
+    Spawn = 0,
+    Extension = 1,
+    Road = 2,
+    Wall = 3,
+    Rampart = 4,
+    KeeperLair = 5,
+    Portal = 6,
+    Controller = 7,
+    Link = 8,
+    Storage = 9,
+    Tower = 10,
+    Observer = 11,
+    PowerBank = 12,
+    PowerSpawn = 13,
+    Extractor = 14,
+    Lab = 15,
+    Terminal = 16,
+    Container = 17,
+    Nuker = 18,
+}
+
+impl StructureType {
+    /// Translates the `CONSTRUCTION_COST` constant.
+    #[inline]
+    pub fn construction_cost(self) -> Option<u32> {
+        use self::StructureType::*;
+
+        let cost = match self {
+            Spawn => 15_000,
+            Extension => 3_000,
+            Road => 300,
+            Wall => 1,
+            Rampart => 1,
+            Link => 5_000,
+            Storage => 30_000,
+            Tower => 5_000,
+            Observer => 8_000,
+            PowerSpawn => 100_000,
+            Extractor => 5_000,
+            Lab => 50_000,
+            Terminal => 100_000,
+            Container => 5_000,
+            Nuker => 100_000,
+            KeeperLair | PowerBank | Portal | Controller => return None,
+        };
+        Some(cost)
+    }
+
+    #[inline]
+    pub fn initial_hits(self) -> Option<u32> {
+        use self::StructureType::*;
+        use super::numbers::*;
+
+        let hits = match self {
+            Spawn => SPAWN_HITS,
+            Extension => EXTENSION_HITS,
+            Road => ROAD_HITS,
+            Wall => WALL_HITS,
+            Rampart => RAMPART_HITS,
+            Link => LINK_HITS,
+            Storage => STORAGE_HITS,
+            Tower => TOWER_HITS,
+            Observer => OBSERVER_HITS,
+            PowerBank => POWER_BANK_HITS,
+            PowerSpawn => POWER_SPAWN_HITS,
+            Extractor => EXTENSION_HITS,
+            Lab => LAB_HITS,
+            Terminal => TOWER_HITS,
+            Container => CONTAINER_HITS,
+            Nuker => NUKER_HITS,
+            KeeperLair | Portal | Controller => return None,
+        };
+        Some(hits)
+    }
+
+    /// Helper function for deserializing from a string rather than a fake
+    /// integer value.
+    pub fn deserialize_from_str<'de, D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let s: Cow<'de, str> = Cow::deserialize(d)?;
+        Self::from_str(&s).map_err(|_| {
+            D::Error::invalid_value(Unexpected::Str(&s), &"a known STRUCTURE_* constant string")
+        })
+    }
+}
+
+js_deserializable!(StructureType);
+
+/// Translates `SUBSCRIPTION_TOKEN` and `INTERSHARD_RESOURCES` constants.
+///
+/// *Note:* This constant's `TryFrom<Value>`, `Serialize` and `Deserialize`
+/// implementations only operate on made-up integer constants. If you're ever
+/// using these impls manually, use the `__intershard_resource_type_num_to_str`
+/// and `__intershard_resource_type_str_to_num` JavaScript functions,
+/// [`FromStr`][std::str::FromStr] or
+/// [`IntershardResourceType::deserialize_from_str`].
+///
+/// See the [module-level documentation][crate::constants] for more details.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize_repr, Deserialize_repr, FromStr)]
+#[repr(u8)]
+pub enum IntershardResourceType {
+    #[display("token")]
+    SubscriptionToken = 1,
+}
+
+impl IntershardResourceType {
+    /// Helper function for deserializing from a string rather than a fake
+    /// integer value.
+    pub fn deserialize_from_str<'de, D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let s: Cow<'de, str> = Cow::deserialize(d)?;
+        Self::from_str(&s).map_err(|_| {
+            D::Error::invalid_value(
+                Unexpected::Str(&s),
+                &"a known constant string in INTERSHARD_RESOURCES",
+            )
+        })
+    }
+}
+
+js_deserializable!(IntershardResourceType);
+
+/// Resource type constant for all possible types of resources.
+///
+/// *Note:* This constant's `TryFrom<Value>`, `Serialize` and `Deserialize`
+/// implementations only operate on made-up integer constants. If you're ever
+/// using these impls manually, use the `__resource_type_num_to_str`
+/// and `__resource_type_str_to_num` JavaScript functions,
+/// [`FromStr`][std::str::FromStr] or [`ResourceType::deserialize_from_str`].
+///
+/// See the [module-level documentation][crate::constants] for more details.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize_repr, Deserialize_repr, FromStr)]
+#[repr(u16)]
+pub enum ResourceType {
+    /// `"energy"`
+    #[display("energy")]
+    Energy = 1,
+    /// `"power"`
+    #[display("power")]
+    Power = 2,
+    /// `"H"`
+    #[display("H")]
+    Hydrogen = 3,
+    /// `"O"`
+    #[display("O")]
+    Oxygen = 4,
+    /// `"U"`
+    #[display("U")]
+    Utrium = 5,
+    /// `"L"`
+    #[display("L")]
+    Lemergium = 6,
+    /// `"K"`
+    #[display("K")]
+    Keanium = 7,
+    /// `"Z"`
+    #[display("Z")]
+    Zynthium = 8,
+    /// `"X"`
+    #[display("X")]
+    Catalyst = 9,
+    /// `"G"`
+    #[display("G")]
+    Ghodium = 10,
+    /// `"OH"`
+    #[display("OH")]
+    Hydroxide = 11,
+    /// `"ZK"`
+    #[display("ZK")]
+    ZynthiumKeanite = 12,
+    /// `"UL"`
+    #[display("UL")]
+    UtriumLemergite = 13,
+    /// `"UH"`
+    #[display("UH")]
+    UtriumHydride = 14,
+    /// `"UO"`
+    #[display("UO")]
+    UtriumOxide = 15,
+    /// `"KH"`
+    #[display("KH")]
+    KeaniumHydride = 16,
+    /// `"KO"`
+    #[display("KO")]
+    KeaniumOxide = 17,
+    /// `"LH"`
+    #[display("LH")]
+    LemergiumHydride = 18,
+    /// `"LO"`
+    #[display("LO")]
+    LemergiumOxide = 19,
+    /// `"ZH"`
+    #[display("ZH")]
+    ZynthiumHydride = 20,
+    /// `"ZO"`
+    #[display("ZO")]
+    ZynthiumOxide = 21,
+    /// `"GH"`
+    #[display("GH")]
+    GhodiumHydride = 22,
+    /// `"GO"`
+    #[display("GO")]
+    GhodiumOxide = 23,
+    /// `"UH2O"`
+    #[display("UH2O")]
+    UtriumAcid = 24,
+    /// `"UHO2"`
+    #[display("UHO2")]
+    UtriumAlkalide = 25,
+    /// `"KH2O"`
+    #[display("KH2O")]
+    KeaniumAcid = 26,
+    /// `"KHO2"`
+    #[display("KHO2")]
+    KeaniumAlkalide = 27,
+    /// `"LH2O"`
+    #[display("LH2O")]
+    LemergiumAcid = 28,
+    /// `"LHO2"`
+    #[display("LHO2")]
+    LemergiumAlkalide = 29,
+    /// `"ZH2O"`
+    #[display("ZH2O")]
+    ZynthiumAcid = 30,
+    /// `"ZHO2"`
+    #[display("ZHO2")]
+    ZynthiumAlkalide = 31,
+    /// `"GH2O"`
+    #[display("GH2O")]
+    GhodiumAcid = 32,
+    /// `"GHO2"`
+    #[display("GHO2")]
+    GhodiumAlkalide = 33,
+    /// `"XUH2O"`
+    #[display("XUH2O")]
+    CatalyzedUtriumAcid = 34,
+    /// `"XUHO2"`
+    #[display("XUHO2")]
+    CatalyzedUtriumAlkalide = 35,
+    /// `"XKH2O"`
+    #[display("XKH2O")]
+    CatalyzedKeaniumAcid = 36,
+    /// `"XKHO2"`
+    #[display("XKHO2")]
+    CatalyzedKeaniumAlkalide = 37,
+    /// `"XLH2O"`
+    #[display("XLH2O")]
+    CatalyzedLemergiumAcid = 38,
+    /// `"XLHO2"`
+    #[display("XLHO2")]
+    CatalyzedLemergiumAlkalide = 39,
+    /// `"XZH2O"`
+    #[display("XZH2O")]
+    CatalyzedZynthiumAcid = 40,
+    /// `"XZHO2"`
+    #[display("XZHO2")]
+    CatalyzedZynthiumAlkalide = 41,
+    /// `"XGH2O"`
+    #[display("XGH2O")]
+    CatalyzedGhodiumAcid = 42,
+    /// `"XGHO2"`
+    #[display("XGHO2")]
+    CatalyzedGhodiumAlkalide = 43,
+    /// `"ops"`
+    #[display("ops")]
+    Ops = 44,
+}
+
+impl ResourceType {
+    /// Translates the `REACTION_TIME` constant.
+    #[inline]
+    pub fn reaction_time(self) -> Option<u32> {
+        use crate::ResourceType::*;
+        let time = match self {
+            Energy | Power | Hydrogen | Oxygen | Utrium | Lemergium | Keanium | Zynthium
+            | Catalyst | Ops => return None,
+            // these comments copied directly from JavaScript 'constants.js' file.
+            // OH: 20,
+            Hydroxide => 20,
+            // ZK: 5,
+            ZynthiumKeanite => 5,
+            // UL: 5,
+            UtriumLemergite => 5,
+            // G: 5,
+            Ghodium => 5,
+            // UH: 10,
+            UtriumHydride => 10,
+            // UH2O: 5,
+            UtriumAcid => 5,
+            // XUH2O: 60,
+            CatalyzedUtriumAcid => 60,
+            // UO: 10,
+            UtriumOxide => 10,
+            // UHO2: 5,
+            UtriumAlkalide => 5,
+            // XUHO2: 60,
+            CatalyzedUtriumAlkalide => 60,
+            // KH: 10,
+            KeaniumHydride => 10,
+            // KH2O: 5,
+            KeaniumAcid => 5,
+            // XKH2O: 60,
+            CatalyzedKeaniumAcid => 60,
+            // KO: 10,
+            KeaniumOxide => 10,
+            // KHO2: 5,
+            KeaniumAlkalide => 5,
+            // XKHO2: 60,
+            CatalyzedKeaniumAlkalide => 60,
+            // LH: 15,
+            LemergiumHydride => 15,
+            // LH2O: 10,
+            LemergiumAcid => 10,
+            // XLH2O: 65,
+            CatalyzedLemergiumAcid => 65,
+            // LO: 10,
+            LemergiumOxide => 10,
+            // LHO2: 5,
+            LemergiumAlkalide => 5,
+            // XLHO2: 60,
+            CatalyzedLemergiumAlkalide => 60,
+            // ZH: 20,
+            ZynthiumHydride => 20,
+            // ZH2O: 40,
+            ZynthiumAcid => 40,
+            // XZH2O: 160,
+            CatalyzedZynthiumAcid => 160,
+            // ZO: 10,
+            ZynthiumOxide => 10,
+            // ZHO2: 5,
+            ZynthiumAlkalide => 5,
+            // XZHO2: 60,
+            CatalyzedZynthiumAlkalide => 60,
+            // GH: 10,
+            GhodiumHydride => 10,
+            // GH2O: 15,
+            GhodiumAcid => 15,
+            // XGH2O: 80,
+            CatalyzedGhodiumAcid => 80,
+            // GO: 10,
+            GhodiumOxide => 10,
+            // GHO2: 30,
+            GhodiumAlkalide => 30,
+            // XGHO2: 150,
+            CatalyzedGhodiumAlkalide => 150,
+        };
+        Some(time)
+    }
+
+    /// Helper function for deserializing from a string rather than a fake
+    /// integer value.
+    pub fn deserialize_from_str<'de, D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
+        let s: Cow<'de, str> = Cow::deserialize(d)?;
+        Self::from_str(&s).map_err(|_| {
+            D::Error::invalid_value(
+                Unexpected::Str(&s),
+                &"a known constant string in RESOURCES_ALL",
+            )
+        })
+    }
+}
+
+js_deserializable!(ResourceType);
+
+/// Translates the `PWR_*` constants.
+#[derive(
+    Copy, Clone, Debug, PartialEq, Eq, Hash, FromPrimitive, Serialize_repr, Deserialize_repr,
+)]
+#[repr(u8)]
+pub enum PowerType {
+    GenerateOps = 1,
+    OperateSpawn = 2,
+    OperateTower = 3,
+    OperateStorage = 4,
+    OperateLab = 5,
+    OperateExtension = 6,
+    OperateObserve = 7,
+    OperateTerminal = 8,
+    DisruptSpawn = 9,
+    DisruptTower = 10,
+    Shield = 12,
+    RegenSource = 13,
+    RegenMineral = 14,
+    DisruptTerminal = 15,
+    OperatePower = 16,
+    Fortify = 17,
+    OperateController = 18,
+    OperateFactory = 19,
+}
+
+js_deserializable!(PowerType);
