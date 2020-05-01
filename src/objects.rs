4964b50aedbe66371b29bb921048c2929eb0769f
Shane Madden <shanemadden@fake.email>
2020-05-01 16:16:38 -0600

    Use an i32 for the free capacity to handle cases where it's negative

--- src/objects.rs
+++ src/objects.rs
@@ -366,12 +366,12 @@ pub unsafe trait HasStore: RoomObjectProperties {
         }
     }
 
-    fn store_free_capacity(&self, resource: Option<ResourceType>) -> u32 {
+    fn store_free_capacity(&self, resource: Option<ResourceType>) -> i32 {
         match resource {
             Some(ty) => {
-                js_unwrap!(Math.max(0, @{self.as_ref()}.store.getFreeCapacity(__resource_type_num_to_str(@{ty as u32})) || 0))
+                js_unwrap!(@{self.as_ref()}.store.getFreeCapacity(__resource_type_num_to_str(@{ty as u32})))
             }
-            None => js_unwrap!(Math.max(0, @{self.as_ref()}.store.getFreeCapacity() || 0)),
+            None => js_unwrap!(@{self.as_ref()}.store.getFreeCapacity()),
         }
     }
 
