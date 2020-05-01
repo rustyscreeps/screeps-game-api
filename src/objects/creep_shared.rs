8c3f33f491892d28a6f53e58fc1cad62f57e64e7
William Archbell <william@archbell.com>
2020-04-30 20:10:19 -0700

    Add helper methods for generating visuals. Add creep path finding visualization.

--- src/objects/creep_shared.rs
+++ src/objects/creep_shared.rs
@@ -8,7 +8,7 @@ use crate::{
     memory::MemoryReference,
     objects::{
         Creep, FindOptions, HasPosition, PowerCreep, Resource, RoomObjectProperties, Step,
-        Transferable, Withdrawable,
+        Transferable, Withdrawable, PolyStyle
     },
     pathfinder::{MultiRoomCostResult, CostMatrix, SearchResults},
     ConversionError,
@@ -75,7 +75,7 @@ pub unsafe trait SharedCreepProperties: RoomObjectProperties {
             reuse_path,
             serialize_memory,
             no_path_finding,
-            // visualize_path_style,
+            visualize_path_style,
             find_options:
                 FindOptions {
                     ignore_creeps,
@@ -90,7 +90,6 @@ pub unsafe trait SharedCreepProperties: RoomObjectProperties {
                     swamp_cost,
                     ..
                 },
-            phantom: PhantomData
         } = move_options;
 
         let mut raw_callback = cost_callback;
@@ -132,7 +131,7 @@ pub unsafe trait SharedCreepProperties: RoomObjectProperties {
                     reusePath: @{reuse_path},
                     serializeMemory: @{serialize_memory},
                     noPathFinding: @{no_path_finding},
-                    visualizePathStyle: undefined,  // todo
+                    visualizePathStyle: @{visualize_path_style},
                     ignoreCreeps: @{ignore_creeps},
                     ignoreDestructibleStructures: @{ignore_destructible_structures},
                     costCallback: cb,
@@ -262,9 +261,8 @@ where
     pub(crate) reuse_path: u32,
     pub(crate) serialize_memory: bool,
     pub(crate) no_path_finding: bool,
-    // pub(crate) visualize_path_style: PolyStyle,
+    pub(crate) visualize_path_style: Option<PolyStyle>,
     pub(crate) find_options: FindOptions<'a, F, MultiRoomCostResult<'a>>,
-    phantom: PhantomData<&'a ()>
 }
 
 impl Default
@@ -277,9 +275,8 @@ impl Default
             reuse_path: 5,
             serialize_memory: true,
             no_path_finding: false,
-            // visualize_path_style: None,
+            visualize_path_style: None,
             find_options: FindOptions::default(),
-            phantom: PhantomData
         }
     }
 }
@@ -313,11 +310,11 @@ where
         self
     }
 
-    // /// Sets the style to trace the path used by this creep. See doc for default.
-    // pub fn visualize_path_style(mut self, style: ) -> Self {
-    //     self.visualize_path_style = style;
-    //     self
-    // }
+    /// Sets the style to trace the path used by this creep. See doc for default.
+    pub fn visualize_path_style(mut self, style: PolyStyle) -> Self {
+        self.visualize_path_style = Some(style);
+        self
+    }
 
     /// Sets whether the algorithm considers creeps as walkable. Default: False.
     pub fn ignore_creeps(mut self, ignore: bool) -> Self {
@@ -341,9 +338,8 @@ where
             reuse_path: self.reuse_path,
             serialize_memory: self.serialize_memory,
             no_path_finding: self.no_path_finding,
-            // self.visualize_path_style,
+            visualize_path_style: self.visualize_path_style,
             find_options: self.find_options.cost_callback(cost_callback),
-            phantom: PhantomData
         };
 
         new_options
@@ -399,9 +395,8 @@ where
             reuse_path: self.reuse_path,
             serialize_memory: self.serialize_memory,
             no_path_finding: self.no_path_finding,
-            // self.visualize_path_style,
+            visualize_path_style: self.visualize_path_style,
             find_options,
-            phantom: PhantomData
         }
     }
 }
