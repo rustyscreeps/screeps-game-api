f9d162c65909cd575e659fa5af42a430a5a7271c
Shane Madden <shanemadden@fake.email>
2020-12-02 18:59:03 -0700

    Update mechanics comments

--- src/constants/seasonal.rs
+++ src/constants/seasonal.rs
@@ -9,10 +9,16 @@
 /// [`ScoreCollector`]: crate::objects::ScoreCollector
 #[cfg(feature = "enable-score")]
 pub const WALLS_RADIUS: u32 = 5;
-// todo mechanics once they're public
+/// The percentage chance that a given room will have a [`ScoreContainer`]
+/// spawned in it every [`SCORE_CONTAINER_SPAWN_INTERVAL`] ticks.
+///
+/// [`ScoreContainer`]: crate::objects::ScoreContainer
 #[cfg(feature = "enable-score")]
 pub const SCORE_CONTAINER_SPAWN_CHANCE: f32 = 0.01;
-// todo mechanics once they're public
+/// The number of ticks between chances to spawn a [`ScoreContainer`] in rooms
+/// at random.
+///
+/// [`ScoreContainer`]: crate::objects::ScoreContainer
 #[cfg(feature = "enable-score")]
 pub const SCORE_CONTAINER_SPAWN_INTERVAL: u32 = 500;
 /// Amount of capacity in a [`ScoreCollector`] that regenerates each tick.
@@ -20,8 +26,10 @@ pub const SCORE_CONTAINER_SPAWN_INTERVAL: u32 = 500;
 /// [`ScoreCollector`]: crate::objects::ScoreCollector
 #[cfg(feature = "enable-score")]
 pub const SCORE_COLLECTOR_SINK: u32 = 20;
-/// Maximum capacity bucket for a [`ScoreCollector`]
+/// Maximum capacity bucket for a [`ScoreCollector`].
 ///
 /// [`ScoreCollector`]: crate::objects::ScoreCollector
 #[cfg(feature = "enable-score")]
 pub const SCORE_COLLECTOR_MAX_CAPACITY: u32 = 20_000;
+// possible todo, Seasonal season 1 extras:
+// https://github.com/screeps/mod_season1/blob/fef5c8e39904ae150b0ae396028fa5faf51cc5a5/src/scoreContainer.roomObject.js#L1
