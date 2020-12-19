14d3b343aa77f8a13d354b7d4120bc013ea5ecf0
Shane Madden <shanemadden@fake.email>
2020-12-19 13:55:39 -0700

    Update seasonal constants to match stdweb branch

--- src/constants/seasonal.rs
+++ src/constants/seasonal.rs
@@ -1,35 +1,40 @@
 //! Constants that apply only to Screeps seasonal servers.
 //!
 //! [Screeps seasonal documentation](https://docs-season.screeps.com/api/#Season-constants)
-// Season 1's RESOURCE_SCORE defined in `types.rs`
-// Season 1's FIND_SCORE_CONTAINERS and FIND_SCORE_COLLECTORS defined in `small_enums.rs`
-/// Radius around each [`ScoreCollector`] that starts surrounded with
-/// constructed walls with random hit points.
+
+/// Constants for Screeps seasonal, season 1
 ///
-/// [`ScoreCollector`]: crate::objects::ScoreCollector
-#[cfg(feature = "enable-score")]
-pub const WALLS_RADIUS: u32 = 5;
-/// The percentage chance that a given room will have a [`ScoreContainer`]
-/// spawned in it every [`SCORE_CONTAINER_SPAWN_INTERVAL`] ticks.
-///
-/// [`ScoreContainer`]: crate::objects::ScoreContainer
-#[cfg(feature = "enable-score")]
-pub const SCORE_CONTAINER_SPAWN_CHANCE: f32 = 0.01;
-/// The number of ticks between chances to spawn a [`ScoreContainer`] in rooms
-/// at random.
-///
-/// [`ScoreContainer`]: crate::objects::ScoreContainer
-#[cfg(feature = "enable-score")]
-pub const SCORE_CONTAINER_SPAWN_INTERVAL: u32 = 500;
-/// Amount of capacity in a [`ScoreCollector`] that regenerates each tick.
-///
-/// [`ScoreCollector`]: crate::objects::ScoreCollector
-#[cfg(feature = "enable-score")]
-pub const SCORE_COLLECTOR_SINK: u32 = 20;
-/// Maximum capacity bucket for a [`ScoreCollector`].
-///
-/// [`ScoreCollector`]: crate::objects::ScoreCollector
-#[cfg(feature = "enable-score")]
-pub const SCORE_COLLECTOR_MAX_CAPACITY: u32 = 20_000;
-// possible todo, Seasonal season 1 extras:
-// https://github.com/screeps/mod_season1/blob/fef5c8e39904ae150b0ae396028fa5faf51cc5a5/src/scoreContainer.roomObject.js#L1
+/// These constants are relevant to the mechanics in seasonal, season 1.
+/// [This mod](https://github.com/screeps/mod_season1) can be used to enable these mechanics
+/// on a private server.
+#[cfg(feature = "seasonal-season-1")]
+pub mod season_1 {
+    // RESOURCE_SCORE defined in `types.rs`
+    // FIND_SCORE_CONTAINERS defined in `find.rs`
+    /// Radius around each [`ScoreCollector`] that starts surrounded with
+    /// [`StructureWall`]s with random hit points.
+    ///
+    /// [`ScoreCollector`]: crate::objects::ScoreCollector
+    /// [`StructureWall`]: crate::objects::StructureWall
+    pub const WALLS_RADIUS: u8 = 5;
+    /// The percentage chance that a given room will have a [`ScoreContainer`]
+    /// spawned in it every [`SCORE_CONTAINER_SPAWN_INTERVAL_TICKS`] ticks.
+    ///
+    /// [`ScoreContainer`]: crate::objects::ScoreContainer
+    pub const SCORE_CONTAINER_SPAWN_CHANCE: f32 = 0.01;
+    // SCORE_CONTAINER_SPAWN_INTERVAL not implemented due to being no longer
+    // used as of https://github.com/screeps/mod-season1/commit/3b4d0aaabfb4bffab80845ac4ea9611f83935e1c
+    /// The number of ticks between chances to spawn a [`ScoreContainer`] in
+    /// rooms at random.
+    ///
+    /// [`ScoreContainer`]: crate::objects::ScoreContainer
+    pub const SCORE_CONTAINER_SPAWN_INTERVAL_TICKS: u32 = 250;
+    /// Amount of capacity in a [`ScoreCollector`] that regenerates each tick.
+    ///
+    /// [`ScoreCollector`]: crate::objects::ScoreCollector
+    pub const SCORE_COLLECTOR_SINK: u32 = 20;
+    /// Maximum capacity bucket for a [`ScoreCollector`].
+    ///
+    /// [`ScoreCollector`]: crate::objects::ScoreCollector
+    pub const SCORE_COLLECTOR_MAX_CAPACITY: u32 = 20_000;
+}
