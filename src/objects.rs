e240426bf1d25693f76c6823b76d1f976fd92f02
Shane Madden <shanemadden@users.noreply.github.com>
2023-03-30 11:13:32 -0600

    First crack at making the docs for crate::objects usable

--- src/objects.rs
+++ src/objects.rs
@@ -1,30 +1,62 @@
 //! Screeps object wrappers.
 mod impls;
 
-pub use self::impls::{
-    AccountPowerCreep, AttackEvent, AttackType, BodyPart, BuildEvent, ConstructionSite, CostMatrix,
-    CostMatrixSet, Creep, Deposit, Effect, Event, EventType, ExitEvent, FindPathOptions, Flag,
-    HarvestEvent, HasLocalPosition, HealEvent, HealType, InterShardPortalDestination,
-    JsFindPathOptions, Mineral, MoveToOptions, Nuke, ObjectDestroyedEvent, OwnedStructure, Owner,
-    Path, PortalDestination, PowerCreep, PowerEvent, PowerInfo, RepairEvent, Reservation,
-    ReserveControllerEvent, Resource, Room, RoomObject, RoomPosition, RoomTerrain, Ruin, Sign,
-    Source, SpawnOptions, Spawning, Step, Store, Structure, StructureContainer,
-    StructureController, StructureExtension, StructureExtractor, StructureFactory,
-    StructureInvaderCore, StructureKeeperLair, StructureLab, StructureLink, StructureNuker,
-    StructureObserver, StructurePortal, StructurePowerBank, StructurePowerSpawn, StructureRampart,
-    StructureRoad, StructureSpawn, StructureStorage, StructureTerminal, StructureTower,
-    StructureWall, Tombstone, TransferEvent, UpgradeControllerEvent,
-};
+pub use event::*;
+pub use game_types::*;
+pub use generic::*;
+pub use room_objects::*;
+pub use visual::*;
 
-#[cfg(feature = "score")]
-pub use self::impls::{ScoreCollector, ScoreContainer};
+/// Object wrappers representing data retrieved from room event logs
+pub mod event {
+    pub use super::impls::{
+        AttackEvent, AttackType, BuildEvent, Event, EventType, ExitEvent, HarvestEvent, HealEvent,
+        HealType, ObjectDestroyedEvent, PowerEvent, RepairEvent, ReserveControllerEvent,
+        TransferEvent, UpgradeControllerEvent,
+    };
+}
 
-#[cfg(feature = "symbols")]
-pub use self::impls::{SymbolContainer, SymbolDecoder};
+/// Object wrappers for game types that are not room objects and are safe to use
+/// in future ticks.
+pub mod game_types {
+    pub use super::impls::{CostMatrix, RoomPosition, RoomTerrain};
+}
 
-pub use self::impls::{
-    CircleStyle, FontStyle, LineDrawStyle, LineStyle, PolyStyle, RectStyle, RoomVisual, TextAlign,
-    TextStyle, Visual,
-};
+/// Object wrappers for simple javascript objects with known properties sent to
+/// or returned by game functions
+pub mod generic {
+    pub use super::impls::{
+        AccountPowerCreep, BodyPart, CostMatrixSet, Effect, FindPathOptions, HasLocalPosition,
+        InterShardPortalDestination, JsFindPathOptions, MoveToOptions, Owner, Path,
+        PortalDestination, PowerInfo, Reservation, Sign, SpawnOptions, Step,
+    };
+}
 
-pub use self::impls::{MapVisual, MapVisualShape};
+/// Object wrappers representing room objects within the game world, which are
+/// unsafe to use in future ticks
+pub mod room_objects {
+    pub use super::impls::{
+        ConstructionSite, Creep, Deposit, Flag, Mineral, Nuke, OwnedStructure, PowerCreep,
+        Resource, Room, RoomObject, Ruin, Source, Spawning, Store, Structure, StructureContainer,
+        StructureController, StructureExtension, StructureExtractor, StructureFactory,
+        StructureInvaderCore, StructureKeeperLair, StructureLab, StructureLink, StructureNuker,
+        StructureObserver, StructurePortal, StructurePowerBank, StructurePowerSpawn,
+        StructureRampart, StructureRoad, StructureSpawn, StructureStorage, StructureTerminal,
+        StructureTower, StructureWall, Tombstone,
+    };
+
+    #[cfg(feature = "score")]
+    pub use super::impls::{ScoreCollector, ScoreContainer};
+
+    #[cfg(feature = "symbols")]
+    pub use super::impls::{SymbolContainer, SymbolDecoder};
+}
+
+/// Object wrappers allowing drawing of shapes in rooms or on the map in the
+/// game world
+pub mod visual {
+    pub use super::impls::{
+        CircleStyle, FontStyle, LineDrawStyle, LineStyle, MapVisual, MapVisualShape, PolyStyle,
+        RectStyle, RoomVisual, TextAlign, TextStyle, Visual,
+    };
+}
