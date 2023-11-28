480f91eff5b1868a8351b870588b4d5eb8e3b754
Shane Madden <shanemadden@users.noreply.github.com>
2023-11-27 18:13:25 -0700

    fmt

--- src/objects/impls/room_terrain.rs
+++ src/objects/impls/room_terrain.rs
@@ -60,11 +60,11 @@ impl RoomTerrain {
     pub fn get_raw_buffer_to_array(&self, destination: &Uint8Array) -> Result<(), ErrorCode> {
         let val = self.get_raw_buffer_to_array_internal(destination);
 
-        // val is integer if error; if object it's another reference to the Uint8Array; function
-        // was successful in that case
+        // val is integer if error; if object it's another reference to the Uint8Array;
+        // function was successful in that case
         match val.as_f64() {
             Some(n) => ErrorCode::result_from_i8(n as i8),
-            None => Ok(())
+            None => Ok(()),
         }
     }
 }
